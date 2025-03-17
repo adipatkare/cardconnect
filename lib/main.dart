af173851b26648a18a165d5a68ecdf04c3bd5c64
Aditya Patkare <110580401+adipatkare@users.noreply.github.com>
2025-03-17 14:40:07 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CardConnect());
+}
+
+class CardConnect extends StatelessWidget {
+  const CardConnect({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Container(),
+
+      ),
+    );
+  }
+}
